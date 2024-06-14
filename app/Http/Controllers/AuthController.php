b16e7de5b6ad0d871c250ae7da1efe9ff723a290
ManuelChale04 <152662614+ManuelChale04@users.noreply.github.com>
2024-06-13 23:37:25 -0600

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,8 +4,60 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            return response()->json([
+                'user' => $user,
+                'authorization' => [
+                    'token' => $user->createToken('ApiToken')->plainTextToken,
+                    'type' => 'bearer',
+                ]
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Datos de acceso erróneos',
+        ], 401);
+    }
+
+    //    public function registrar(Request $request)
+    //    {
+    //        $request->validate([
+    //            'name' => 'required|string|max:255',
+    //            'email' => 'required|string|email|max:255|unique:users',
+    //            'password' => 'required|string|min:6',
+    //        ]);
     //
+    //        $user = User::create([
+    //            'name' => $request->name,
+    //            'email' => $request->email,
+    //            'password' => Hash::make($request->password),
+    //        ]);
+    //
+    //        return response()->json([
+    //            'message' => 'Usuario registrado correctamente',
+    //            'user' => $user
+    //        ]);
+    //    }
+
+    public function logout()
+    {
+        Auth::user()->tokens()->delete();
+        return response()->json([
+            'message' => 'Sesión finalizada',
+        ]);
+    }
 }
